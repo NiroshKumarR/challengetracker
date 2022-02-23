9dadbe0f55397840f703d285c6ca1d1bd0ed6a8d
niroshkumar072 <niroshkumar072@.com>
2022-02-24 04:11:03 +0530

    progress size increaseed

--- src/components/progressbar.js
+++ src/components/progressbar.js
@@ -1,10 +1,11 @@
 export default function Progressbar() {
   const progress = {
     "--value": 20,
+    "--size": "10rem",
   };
   return (
     <div id="progressTracker">
-      <div className="radial-progress  border-4" style={progress}>
+      <div className="radial-progress  border-4 text-2xl" style={progress}>
         {progress["--value"]}%
       </div>
     </div>
