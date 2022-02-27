372d068ad518269fdea1afc9125d7e90857b9a1c
niroshkumar072 <niroshkumar072@.com>
2022-02-27 17:08:03 +0530

    ison attribution added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,21 @@
 import Intro from "../components/intro";
-
+import flaticon from "../assets/flaticon.png";
 export default function Home() {
   return (
     <div id="home" className="container flex flex-col mx-auto lg:px-20 p-5">
-      <div id="name" className="mt-10 lg:ml-72">
+      <div className="flex items-end justify-end">
+        <a href="https://www.flaticon.com/free-icons/" title="Flaticon icons">
+          <img src={flaticon} alt="Flation" className="w-20" />
+        </a>
+      </div>
+      <div id="name" className="lg:ml-72">
         <h1 className="font-semibold lg:text-4xl text-3xl">
           <span className="font-serif lg:text-6xl text-4xl">C</span>hallenge{" "}
           <span className="font-serif lg:text-6xl text-4xl">T</span>racker
         </h1>
         <span className="divider h-1 rounded-lg bg-primary lg:w-1/2"></span>
       </div>
+
       <div id="into">
         <Intro />
       </div>
